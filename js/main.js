2561cb8af1db9ceaae89e105bd9ec3c1cd2e3c75
Elena <elmesheryakova@yandex.ru>
2020-11-27 18:08:58 +0300

    замена бургера на выезжающее меню, добавление блока со шкалой преимуществ

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 //@prepros-append burger.js
 
-$(document).ready(function () {
-	$('.header__burger').click(function (event) {
-		$('.header__burger, .header__menu').toggleClass('burger_active');
-		$('body').toggleClass('lock');
-	});
-});
+//$(document).ready(function () {
+//	$('.header__burger').click(function (event) {
+//		$('.header__burger, .header__menu').toggleClass('burger_active');
+//		$('body').toggleClass('lock');
+//	});
+//});
+$('.menu-btn').on('click', function (e) {
+	e.preventDefault();
+	$('.menu').toggleClass('menu_active');
+	$('.content').toggleClass('content_active');
+})
